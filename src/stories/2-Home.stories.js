d9250f941d2e85fabb6cdcbd5b4b68b7a7f14dd2
Utku Noyan <unoyan16@ku.edu.tr>
2021-03-18 18:16:33 +0300

    Initialize Storybook

---
+++ src/stories/2-Home.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import LoginComponent from "../Components/Authentication/Login.component";
+
+export default {
+  title: "Authentication",
+};
+
+export const LoginPage = () => <LoginComponent>Login</LoginComponent>;
+
