bd3599c92ea2e414abdb03647e71774bb7f90bcd
Furkan Sahbaz <fsahbaz16@ku.edu.tr>
2021-04-01 11:25:03 +0300

    removed unused imports

--- src/Components/Cols/sidebar.js
+++ src/Components/Cols/sidebar.js
@@ -1,11 +1,9 @@
 import React from "react";
 import cn from "classnames";
 import styles from "./sidebar.module.css";
-import TopicsBar from "../TopicsBar/TopicsBar.component";
-import ProfileBox from "../profile-box";
 
-function Sidebar({ flat }) {
-  return <div className={cn(styles.sidebar)}></div>;
+function Sidebar({ children }) {
+  return <div className={cn(styles.sidebar)} />;
 }
 
 export default Sidebar;
