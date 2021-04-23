7578ca754a8bbb5488479c3e1b471dc656f46821
Furkan Sahbaz <fsahbaz16@ku.edu.tr>
2021-04-23 21:38:03 +0300

    minor fix

--- src/Components/TopicsBar/TopicsBar.component.js
+++ src/Components/TopicsBar/TopicsBar.component.js
@@ -8,13 +8,14 @@ function TopicsBarComponent({ children, tags }) {
   return (
     <nav className={styles.nav}>
       <h1 className={styles.title}>Followed Topics</h1>
-      {tags.tags.map((tag) => {
-        return (
-          <Topic key={tag.name} className={styles.topic}>
-            {tag.name}
-          </Topic>
-        );
-      })}
+      {tags &&
+        tags.tags.map((tag) => {
+          return (
+            <Topic key={tag.name} className={styles.topic}>
+              {tag.name}
+            </Topic>
+          );
+        })}
     </nav>
   );
 }
